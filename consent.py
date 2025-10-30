cf38cbf737633bf0568c328fdc68b09c132e8082
Katherine Mudd <katherinemudd@mac.mynetworksettings.com>
2025-10-29 20:37:25 -0400

    working director only experiment

--- consent.py
+++ consent.py
@@ -2,6 +2,7 @@ from psynet.consent import Consent
 from psynet.page import InfoPage
 from markupsafe import Markup
 
+# Old version: <p>If you agree to participate, you will be to be paired up with a partner: you will either have the role of creating sounds on the drum machine or giving feedback on the sounds created by your partner. This will continue over 9 rounds. Finally, you will be asked to answer a short questionnaire about your personal musical abilities and language experience. The amount of time spent in the study is approximately 30 minutes.</p>
 
 class CustomConsent(InfoPage, Consent):
     consent_text = Markup("""
@@ -12,9 +13,9 @@ class CustomConsent(InfoPage, Consent):
 
     <p><strong>Project title: Dynamics of creative exploration</strong></p>
 
-    <p>You are being asked to be in a research study. The purpose of the study is to understand how creativity interacts with using a signal space in the domains of music and communication.</p>
+    <p>You are being asked to be in a research study. The purpose of the study is to understand how creativity interacts with the use of different signal spaces in the domains of music and communication.</p>
 
-    <p>If you agree to participate, your part will be to be paired up with a partner: you will either have the role of creating sounds on the drum machine or giving feedback on the sounds created by your partner. This will continue over 9 rounds. Finally, you will be asked to answer a short questionnaire about your personal musical abilities and language experience. The amount of time spent in the study is approximately 30 minutes.</p>
+    <p>If you agree to participate, you will create rhythms on a drum machine. This will continue over 9 rounds. Finally, you will be asked to answer a short questionnaire about your personal musical abilities and language experience. The amount of time spent in the study is approximately 15 minutes.</p>
 
     <p>All the information about you will be kept private. All the study data that we get from you will be kept secure. If any papers and talks are given about this research, your name will not be used. The information collected during your participation (without any identifiers) may be used for future research studies or distributed to another investigator for future research studies.</p>
 
@@ -26,12 +27,12 @@ class CustomConsent(InfoPage, Consent):
 
     <p>If you have any questions, concerns or complaints about the study, you may contact the Principal Investigator, Margaret Schedel, by telephone (631)-632 7330, or by email at margaret.schedel@stonybrook.edu. If you have any questions about your rights as a research subject or if you would like to obtain information or offer input, you may contact the Stony Brook University Research Subject Advocate, Ms. Lu-Ann Kozlowski, BSN, RN, (631) 632-9036, or by e-mail, lu-ann.kozlowski@stonybrook.edu.</p>
 
-    <p>If you participate in the experiment and questionnaire it means that you have read (or had to read to you) the information in this information sheet, and would like to be a participant.</p>
+    <p>If you participate in the experiment and questionnaire it means that you have read the information in this information sheet, and would like to be a participant.</p>
 
     <p>By clicking onto the next page, you confirm that you give your full informed consent.</p>
     """)
 
-    time_estimate = 60
+    time_estimate = 30
 
     def __init__(self):
         super().__init__(self.consent_text, time_estimate=self.time_estimate)
--- experiment.py
+++ experiment.py
@@ -11,8 +11,15 @@ from psynet.page import InfoPage
 from psynet.timeline import Timeline
 from psynet.trial.static import StaticNetwork, StaticNode, StaticTrial, StaticTrialMaker
 
-from psynet.modular_page import Control, Prompt
-from psynet.timeline import join
+# katie added
+from .consent import CustomConsent
+from .dat import dat
+from dominate import tags
+from psynet.modular_page import Control, Prompt, TextControl
+from psynet.page import SuccessfulEndPage
+from psynet.prescreen import ColorBlindnessTest, AudioForcedChoiceTest
+from psynet.timeline import join, PageMaker
+from .questionnaire import questionnaire
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -39,6 +46,24 @@ class ColorCubeControl(Control):
             "style": "margin-bottom: 0; padding-bottom: 0;"
         }
 
+class DrumMachineControl(Control):
+    macro = "drum_machine"
+    external_template = "custom-controls.html"
+
+    def __init__(self, drum_kit, grid_size, initial_pattern=None):
+        super().__init__()
+        self.drum_kit = drum_kit
+        self.grid_size = grid_size
+        self.initial_pattern = initial_pattern
+
+    @property
+    def metadata(self):
+        return {
+            "drum_kit": self.drum_kit,
+            "grid_size": self.grid_size,
+            "initial_pattern": self.initial_pattern,
+            "style": "margin-bottom: 0; padding-bottom: 0;"
+        }
 
 def get_color_dict():
     return {'yellow': [60, 100, 50],
@@ -53,7 +78,7 @@ def get_color_dict():
 
 
 def get_nodes():
-    domains = ["communication"]
+    domains = ["communication", "music"]
     grid_sizes = [4, 8]
     drum_kits = ["snare+kick", "hihat+snare+kick"]
     blocks = [(d, g, k) for d in domains for g in grid_sizes for k in drum_kits]
@@ -61,7 +86,7 @@ def get_nodes():
     nodes = [
         StaticNode(
             definition={"color": color,
-                        "drum": d,
+                        "domain": d,
                         "grid_size": g,
                         "drum_kit": k},
             block=f"{d}_{g}_{k}"
@@ -73,22 +98,38 @@ def get_nodes():
 
 
 class ColorTrial(StaticTrial):
-    time_estimate = 5
+    time_estimate = 10
 
     def show_trial(self, experiment, participant):
         current_color = self.definition.get("color")
-        color_hsl = get_color_dict().get(current_color)
 
-        return join(ModularPage(
-            "director-task",
-            Prompt(
-                Markup("<div style='text-align:center;'>Make a rhythm to have someone else guess this color</div><br>"),
-                text_align="center"),
-            ColorCubeControl(color_hsl, self.definition["drum_kit"], self.definition["grid_size"]),
-            time_estimate=self.time_estimate,
-            save_answer="last_action"
-        )
-        )
+        if self.definition["domain"] == "communication":
+            color_hsl = get_color_dict().get(current_color)
+
+            return join(
+                ModularPage(
+                "director-task-communication",
+                    Prompt(
+                        Markup("<div style='text-align:center;'>Create a rhythm to have someone else guess this color</div><br>"),
+                        text_align="center"),
+                    ColorCubeControl(color_hsl, self.definition["drum_kit"], self.definition["grid_size"]),
+                    time_estimate=self.time_estimate,
+                    save_answer="last_action"
+                )
+            )
+        else:
+            return join(
+                ModularPage(
+                "director-task-music",
+                    Prompt(
+                        Markup("<div style='text-align:center;'>Create an appealing rhythm.</div><br>"),
+                        text_align="center"
+                    ),
+                    DrumMachineControl(self.definition["drum_kit"], self.definition["grid_size"]),
+                    time_estimate=self.time_estimate,
+                    save_answer="last_action"
+                        )
+                    )
 
 class ColorTrialMaker(StaticTrialMaker):
     def performance_check(self, experiment, participant, participant_trials):
@@ -116,7 +157,7 @@ class ColorTrialMaker(StaticTrialMaker):
         )
 
 
-trial_maker = ColorTrialMaker(
+color_trial_maker = ColorTrialMaker(
     id_="colors",
     trial_class=ColorTrial,
     nodes=get_nodes,
@@ -133,11 +174,118 @@ trial_maker = ColorTrialMaker(
     n_repeat_trials=0,
 )
 
+def requirements():
+    html = tags.div()
+    with html:
+        tags.h1("Experiment structure")
+        tags.p(
+            "Before starting the experiment, here is the structure of the experiment so you know what to expect:"
+        )
+        tags.p(
+            "1. Three short pretests",
+            tags.br(),
+            "2. The main experiment: rhythm creation",
+            tags.br(),
+            "3. A short exit questionnaire"
+        )
+        tags.p(
+            " "
+        )
+        tags.p(
+            "For the duration of the experiment please sit in a quiet place with a good internet connection. You need to be able to hear sounds. The experiment will take approximately 15 minutes."
+        )
+        tags.p(
+            "If you satisfy these requirements, please press 'Next' to begin the experiment. Thank you for taking part in our experiment! If you are not able to satisfy these requirements currently, please exit the experiment."
+        )
+
+    return InfoPage(html, time_estimate=60)
+
+class CustomColorBlindnessTest(ColorBlindnessTest):  # Custom ColorBlindnessTest with custom introduction
+    @property
+    def introduction(self):
+        return InfoPage(
+            tags.div(
+                tags.h1("Color test"),
+                tags.p("This test helps us ensure you can properly see and distinguish colors."),
+                tags.p("You will see colored circles and need to identify the number or pattern within them."),
+                tags.p("In each trial, you will be presented with an image that contains a number."),
+                tags.p(f"This image will disappear after {self.hide_after} seconds."),
+                tags.p("You must enter the number that you see into the text box.")
+            ),
+            time_estimate=10
+        )
+
+class CustomAudioForcedChoiceTest(AudioForcedChoiceTest):  # Custom AudioForcedChoiceTest with custom introduction
+    @property
+    def introduction(self):
+        return InfoPage(
+            tags.div(
+                tags.h1("Audio Test"),
+                tags.p("This test helps us ensure you can properly hear and classify audio stimuli."),
+                tags.p("In each trial, you will hear a sound of an animal. Which animal does this sound come from?"),
+                tags.p("Select the category which fits best to the played sound file.")
+            ),
+            time_estimate=10
+        )
+
+def debrief_and_feedback():
+    return ModularPage(
+        "debrief_and_feedback",
+        Markup("""
+        <h1>Debrief and Feedback</h1>
+        <p>Thank you for participating in this study. The data you have submitted will be used to better understand the relationship between (divergent) creativity, exploration and success on the musical or communication task. Your data will be kept strictly anonymous. If you would like to provide feedback on your experience with this experiment, please type it in the box below.</p>
+        """),
+        TextControl(one_line=False),
+        time_estimate=60
+    )
+
+def redirect_to_prolific():
+    html = tags.div()
+    with html:
+        tags.p(
+            """You will now be redirected directly to the prolific page.""",
+        )
+        tags.script(
+            """
+            setTimeout(function() {
+                window.location.href = 'https://app.prolific.com/submissions/complete?cc=CQCDYRO7';
+            }, 2000);
+            """
+        )
+
+    return InfoPage(html, time_estimate=60)
+
 
 class Exp(psynet.experiment.Experiment):
-    label = "Static experiment demo"
+    label = "Signal Space Experiment"
     initial_recruitment_size = 1
 
     timeline = Timeline(
-        trial_maker,
+        CustomConsent(),
+        PageMaker(requirements, time_estimate=60),
+        CustomColorBlindnessTest(
+             label="color_blindness_test",
+             performance_threshold=1,  # Participants can make 1 mistake
+             time_estimate_per_trial=5.0,
+             hide_after=3.0,  # Image disappears after 3 seconds
+        ),
+        CustomAudioForcedChoiceTest(
+             csv_path="cats_dogs_birds.csv",
+             answer_options=["cat", "dog", "bird"],
+             performance_threshold=1,  # Participants can make 1 mistake
+             instructions="""
+                 <p>This test helps us ensure you can properly hear and classify audio stimuli.</p>
+                 <p>In each trial, you will hear a sound of an animal. Which animal does this sound come from?</p>
+                 """,
+             question="Select the category which fits best to the played sound file.",
+             label="audio_forced_choice_test",
+             time_estimate_per_trial=8.0,
+             n_stimuli_to_use=3  # Use 3 random stimuli from the CSV
+        ),
+        dat(),
+        color_trial_maker,
+        questionnaire(),
+        debrief_and_feedback(),
+        redirect_to_prolific(),
+        SuccessfulEndPage(),
     )
